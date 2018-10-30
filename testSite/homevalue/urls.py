ff7914f3121d58df3c0574f6b352b7355e516efa
MattMont <matthew.montgomery23@gmail.com>
2018-10-29 20:21:55 -0600

    add urls, .html and linds to CTA buttons

--- testSite/homevalue/urls.py
+++ testSite/homevalue/urls.py
@@ -7,5 +7,12 @@ urlpatterns = [
     # path('test/', views.homeAutoComplete.as_view(), name="homeAutoComplete"),
     # path('test/<address>', views.singleView, name='sView'),
     # path('get_addy/', views.get_addy, name='get_addy'),
-    path('', views.indexAuto, name='Address')
+    path('', views.indexAuto, name='Address'),
+
+    #/homevalue/speak-with-agent
+    path('speak-with-agent', views.speakAgent, name ='speakAgent'),
+
+    #/homevalue/list-wtih-us
+    path('list-with-us', views.listWith, name ='listWith')
+
     ]
